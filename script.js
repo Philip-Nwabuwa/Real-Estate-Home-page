4b9f7549af1f402b2b03612198e2342bbb85aac3
Philip John <95682231+Philipjoe21@users.noreply.github.com>
2022-08-03 21:52:58 +0100

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+let proprtyItems = {
+    "Property Type": {
+        "one bedroom": "two bedroom"
+    },
+    "Location": {
+        "lagos": "abuja"
+    },
+    "Price" : number,
+        N3000: N4000
+}
+
+window.onload = function() {
+    let propertySel = document.getElementById("property_type")
+}
